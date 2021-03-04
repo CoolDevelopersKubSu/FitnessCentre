6b7db5f37330621f67d322ebad3d7faf17fefacc
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-03-04 21:22:21 +0300

    Test output table #1

---
+++ View/View/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FitnessCenter;
+
+namespace View
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {    
+        public class columnName
+        {
+            public string name { get; set; }
+            public string card { get; set; }
+        }
+        public void ShowUsers()
+        {
+            //string[][] records = new string[1][];
+            //records[0] = new string[] { "id", "name", "dfdf", "652143" };
+            List<string[]> records = UserController.Users();
+            for (int i=0; i<records.Count; i++)
+            {
+                allUsers_table.Items.Add(new columnName()
+                {
+
+                    name = records[i][0] + " " + records[i][1],
+                    card = records[i][2],
+                }); 
+            }          
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShowUsers();
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Visibility = Visibility.Visible;
+            allUsers_table.Visibility = Visibility.Hidden;
+            allUsers_table.IsEnabled = false;
+            add.Visibility = Visibility.Hidden;
+            add.IsEnabled = false;
+            back.Visibility = Visibility.Visible;
+            frame.Navigate(new Add());
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {          
+            frame.Visibility = Visibility.Hidden;
+            allUsers_table.Visibility = Visibility.Visible;
+            allUsers_table.IsEnabled = true;
+            add.Visibility = Visibility.Visible;
+            add.IsEnabled = true;
+            back.Visibility = Visibility.Hidden;          
+        }
+
+        private void add_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var bc = new BrushConverter();
+            add.Foreground = (Brush)bc.ConvertFrom("#222831");
+        }
+
+        private void add_MouseLeave(object sender, MouseEventArgs e)
+        {
+            var bc = new BrushConverter();
+            add.Foreground = (Brush)bc.ConvertFrom("#FFEEEEEE");
+            add.Background = (Brush)bc.ConvertFrom("#222831");
+        }
+
+        private void back_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var bc = new BrushConverter();
+            add.Foreground = (Brush)bc.ConvertFrom("#222831");
+        }
+
+        private void back_MouseLeave(object sender, MouseEventArgs e)
+        {
+            var bc = new BrushConverter();
+            add.Foreground = (Brush)bc.ConvertFrom("#FFEEEEEE");
+            add.Background = (Brush)bc.ConvertFrom("#222831");
+        }
+    }
+}
---
+++ View/View/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace FitnessCenter
+{
+
+
+    class UserController
+    {
+        protected static List<string[]> RunRequests(string sql, MySqlConnection connection)
+        {
+            connection.Open();
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            MySqlDataReader reader = command.ExecuteReader();
+
+            List<string[]> data = new List<string[]>();
+
+            while (reader.Read())
+            {
+                data.Add(new string[9]);
+
+                data[data.Count - 1][0] = reader[0].ToString();
+                data[data.Count - 1][1] = reader[1].ToString();
+                data[data.Count - 1][2] = reader[2].ToString();
+            }
+            reader.Close();
+            connection.Close();
+            return data;
+        }
+
+        public static List<string[]> Users()
+        {
+            MySqlConnection connection = DBController.GetConnection("127.0.0.1", "fitness_center_bd", 3306, "root", "");
+            string sql = DBController.GetRequest("select Name, Surname, CardNumber");
+            List<string[]> users = RunRequests(sql, connection);
+            return users;
+        }
+    }
+}
