90bc2a3e81d20eeba6995da682d9237e2024cd92
anisimovalinaa <62428300+anisimovalinaa@users.noreply.github.com>
2021-03-04 20:18:20 +0300

    Add

--- View/View/MainWindow.xaml.cs
+++ View/View/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Test;
 
 namespace View
 {
@@ -19,18 +20,36 @@ namespace View
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {    
+        public class columnName
+        {
+            public string name { get; set; }
+            public string card { get; set; }
+        }
+        public void ShowUsers()
+        {
+            string[][] records = new string[1][];
+            records[0] = new string[] { "id", "name", "dfdf", "652143" };
+            for (int i=0; i<records.Length; i++)
+            {
+                allUsers_table.Items.Add(new columnName()
+                {
+                    name = records[i][1] + " " + records[i][2],
+                    card = records[i][3],
+                });
+            }          
+        }
         public MainWindow()
         {
             InitializeComponent();
-            
+            ShowUsers();
         }
 
         private void add_Click(object sender, RoutedEventArgs e)
         {
             frame.Visibility = Visibility.Visible;
-            allUsers.Visibility = Visibility.Hidden;
-            allUsers.IsEnabled = false;
+            allUsers_table.Visibility = Visibility.Hidden;
+            allUsers_table.IsEnabled = false;
             add.Visibility = Visibility.Hidden;
             add.IsEnabled = false;
             back.Visibility = Visibility.Visible;
@@ -40,8 +59,8 @@ namespace View
         private void back_Click(object sender, RoutedEventArgs e)
         {          
             frame.Visibility = Visibility.Hidden;
-            allUsers.Visibility = Visibility.Visible;
-            allUsers.IsEnabled = true;
+            allUsers_table.Visibility = Visibility.Visible;
+            allUsers_table.IsEnabled = true;
             add.Visibility = Visibility.Visible;
             add.IsEnabled = true;
             back.Visibility = Visibility.Hidden;          
